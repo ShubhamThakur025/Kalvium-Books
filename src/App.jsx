7db573f2c0dfc51a99095ddd8bf06648b578f7a1
ShubhamThakur025 <thakur0805@gmail.com>
2024-02-06 13:09:27 +0530

    CSS Tailwind tweaked

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
   useEffect(() => {
     axios.get("https://reactnd-books-api.udacity.com/books", { headers: { "Authorization": 'whatever-you-want' } })
       .then((response) => {
+        console.log(response.data.books)
         setBooks(response.data.books)
       })
       .catch((error) => console.log(error))
@@ -19,29 +20,30 @@ function App() {
 
   return (
     <div>
-      <NavBar setSearch={setSearch}/>
+      <NavBar setSearch={setSearch} />
 
       <Routes>
         <Route path='/' />
         <Route path='/Register' element={<Register />} />
       </Routes>
-
-      {books.filter((book) => {
-        if (searchQuery === '') {
-          return book
-        }
-        else if (book.title.toLowerCase().includes(searchQuery.toLowerCase())) {
-          return book
-        }
-      }).map((book, index) => {
-        console.log(book)
-        return (
-          <div key={index} className='book-card'>
-            <div className='w-30'><img src={`${book.imageLinks.smallThumbnail}`} alt="" /></div>
-            <div className='title'>{book.title} </div>
-          </div>
-        )
-      })}
+      <div className='flex flex-wrap items-center justify-center md:p-16'>
+        {books.filter((book) => {
+          if (searchQuery === '') {
+            return book
+          }
+          else if (book.title.toLowerCase().includes(searchQuery.toLowerCase())) {
+            return book
+          }
+        }).map((book, index) => {
+          console.log(book)
+          return (
+            <div key={index} className='bg-slate-100 border rounded text-center md:h-[380px] md:w-[260px] md:p-10 md:h-300px] md:m-4 shadow-lg flex flex-col items-center justify-center md:p-4'>
+              <img className="block"src={`${book.imageLinks.smallThumbnail}`} alt="" />
+              <div className='font-bold md:mt-1.5 md:w-[130px]'>{book.title} </div>
+            </div>
+          ) 
+        })}
+      </div>
       <Footer />
     </div>
   )
