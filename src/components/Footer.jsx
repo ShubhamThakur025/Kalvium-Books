52cd826d7b3cafff354a9f42c3a0ba356e26f446
ShubhamThakur025 <thakur0805@gmail.com>
2024-02-07 09:47:54 +0530

    Links added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom"
 function Footer() {
   return (
     <div className="w-screen text-center min-m-screen bg-black text-white p-5">
-      <div>Designed and Developed by: <a className="underline hover:text-gray-400 mr-[40px]" href="">Shubham Thakur</a></div>
-      <div>API support by: <a className="underline hover:text-gray-400 mt-2" href="#">The API</a></div>
+      <div>Designed and Developed by: <a className="underline hover:text-gray-400 mr-[40px]" target="_blank" href="https://github.com/ShubhamThakur025">Shubham Thakur</a></div>
+      <div>API support by: <a className="underline hover:text-gray-400 mt-2"target="_blank" href="https://reactnd-books-api.udacity.com/">ReactNd-books-udacity</a></div>
     </div>
   )
 }
