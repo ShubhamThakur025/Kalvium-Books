b6132afcdf61827505740a65d6b017416f62f416
ShubhamThakur025 <thakur0805@gmail.com>
2024-02-08 14:25:23 +0530

    Search result status added

--- src/components/BookShelf.jsx
+++ src/components/BookShelf.jsx
@@ -1,28 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import star from '../assets/star.png'
 import Bookdetail from './Bookdetail'
 
 function BookShelf({ books, searchQuery }) {
     const [popup, showPopUp] = useState(false)
     const [selectedBook, changeBook] = useState({})
+    const [status, foundResults] = useState(false)
+
     const handlePopUp = (book) => {
         showPopUp(true)
         changeBook(book)
     }
+    let filteredBooks = books.filter((book) => {
+        if (searchQuery === '') {
+            return book
+        }
+        else if (book.title.toLowerCase().includes(searchQuery.toLowerCase())) {
+            return book
+        }
+    })
+    useEffect(() => {
+        if(filteredBooks.length < 1 && books.length > 0){
+            foundResults(true)
+        }
+    }, [filteredBooks])
+
+
     return (
         <>
-            {popup ? <Bookdetail showPopUp = {showPopUp} selectedBook={selectedBook}/> : null}
+            {popup ? <Bookdetail showPopUp={showPopUp} selectedBook={selectedBook} /> : null}
             <div className='flex flex-wrap items-center justify-center p-16'>
-                {books.filter((book) => {
-                    if (searchQuery === '') {
-                        return book
-                    }
-                    else if (book.title.toLowerCase().includes(searchQuery.toLowerCase())) {
-                        return book
-                    }
-                }).map((book, index) => {
+                {filteredBooks.map((book, index) => {
                     return (
-                        <div key={index} onClick={()=> handlePopUp(book)}
+                        <div key={index} onClick={() => handlePopUp(book)}
                             className='relative bg-slate-100 hover:bg-slate-200 border rounded md:h-[380px] md:w-[260px] md:p-10 p-8 w-[180px] h-[300px] m-4 shadow-lg flex flex-col items-center justify-center md:p-4 cursor-pointer hover:scale-105 md:text-base text-sm'>
                             <img className="block" src={`${book.imageLinks.smallThumbnail}`} alt="" />
                             <div className='font-bold md:mt-1.5 md:w-[130px]'>{book.title} </div>
@@ -35,7 +45,8 @@ function BookShelf({ books, searchQuery }) {
                     )
                 })}
             </div>
-        </>
+            {status ? <div className='text-4xl text-bold mb-[45vh] text-center'>No Search <span className='text-red-600'>Results</span></div>
+                : null}   </>
     )
 }
 
